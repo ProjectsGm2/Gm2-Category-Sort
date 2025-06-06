228b3ace29aba649d601d5ac2b8eb4cbd06df4cf
ProjectsGm2 <projects.gm2web@gmail.com>
2025-06-07 00:49:44 +0530

    Fix sorting by implementing custom orderby handling

--- includes/utilities.php
+++ includes/utilities.php
@@ -21,3 +21,69 @@ if ( ! function_exists( 'gm2_str_contains' ) ) {
         return $needle === '' || strpos( $haystack, $needle ) !== false;
     }
 }
+
+if ( ! function_exists( 'gm2_get_orderby_args' ) ) {
+    /**
+     * Translate WooCommerce orderby values to WP_Query arguments.
+     *
+     * @param string $orderby Orderby string from the request, e.g. "price-desc".
+     * @return array Arguments for WP_Query.
+     */
+    function gm2_get_orderby_args( $orderby ) {
+        $orderby_value = $orderby;
+        $order_dir     = '';
+
+        if ( gm2_str_contains( $orderby, '-' ) ) {
+            list( $orderby_value, $order_dir ) = array_pad( explode( '-', $orderby ), 2, '' );
+        }
+
+        $order_dir = strtoupper( $order_dir );
+        $args      = [];
+
+        switch ( $orderby_value ) {
+            case 'price':
+                $args['meta_key'] = '_price';
+                $args['orderby']  = 'meta_value_num';
+                $args['order']    = $order_dir ? $order_dir : 'ASC';
+                break;
+
+            case 'popularity':
+                $args['meta_key'] = 'total_sales';
+                $args['orderby']  = 'meta_value_num';
+                $args['order']    = 'DESC';
+                break;
+
+            case 'rating':
+                $args['meta_key'] = '_wc_average_rating';
+                $args['orderby']  = 'meta_value_num';
+                $args['order']    = 'DESC';
+                break;
+
+            case 'date':
+                $args['orderby'] = 'date';
+                $args['order']   = $order_dir ? $order_dir : 'DESC';
+                break;
+
+            case 'rand':
+                $args['orderby'] = 'rand';
+                break;
+
+            case 'id':
+                $args['orderby'] = 'ID';
+                $args['order']   = $order_dir ? $order_dir : 'ASC';
+                break;
+
+            case 'title':
+                $args['orderby'] = 'title';
+                $args['order']   = $order_dir ? $order_dir : 'ASC';
+                break;
+
+            default:
+                $args['orderby'] = 'menu_order title';
+                $args['order']   = $order_dir ? $order_dir : 'ASC';
+                break;
+        }
+
+        return $args;
+    }
+}
