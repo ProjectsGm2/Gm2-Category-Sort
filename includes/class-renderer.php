f9f3f336fff39f9adb4a0281d50e55298cdea643
ProjectsGm2 <projects.gm2web@gmail.com>
2025-06-21 22:23:57 +0530

    Add allow multi selection option

--- includes/class-renderer.php
+++ includes/class-renderer.php
@@ -17,7 +17,8 @@ class Gm2_Category_Sort_Renderer {
              data-filter-type="<?= esc_attr($this->settings['filter_type']) ?>"
              data-simple-operator="<?= esc_attr($this->settings['simple_operator'] ?? 'IN') ?>"
              data-columns="<?= esc_attr(wc_get_loop_prop('columns')) ?>"
-             data-per-page="<?= esc_attr(wc_get_loop_prop('per_page')) ?>">
+             data-per-page="<?= esc_attr(wc_get_loop_prop('per_page')) ?>"
+             data-allow-multi="<?= $this->settings['allow_multi_global'] ? 'true' : 'false' ?>">
              
             <nav class="gm2-category-tree">
                 <?php $this->render_category_tree(); ?>
